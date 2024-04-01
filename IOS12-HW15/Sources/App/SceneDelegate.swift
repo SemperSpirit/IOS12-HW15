a187f7b7bf9b39104eb1a8ef52e9f94a3fde4f55
SemperSpirit <kaiten-aiko@mail.ru>
2024-04-01 18:50:44 +0300

    add settings

---
+++ IOS12-HW15/Sources/App/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  IOS12-HW15
+//
+//  Created by Kaiten Aiko on 01.04.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+            guard let windowScene = (scene as? UIWindowScene) else { return }
+            window = UIWindow(windowScene: windowScene)
+            let viewController = ViewController()
+//            let navigationController = UINavigationController(rootViewController: ViewController)
+            window?.rootViewController = viewController
+            window?.makeKeyAndVisible()
+    }
+}
+
