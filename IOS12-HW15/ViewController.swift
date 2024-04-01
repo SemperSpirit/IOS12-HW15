4df8adbfd2285acf516e1d0197cf76399928b294
SemperSpirit <kaiten-aiko@mail.ru>
2024-04-01 18:26:41 +0300

    Initial Commit

---
+++ IOS12-HW15/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS12-HW15
+//
+//  Created by Kaiten Aiko on 01.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
